28dc870d0d8bb5124c302c71b83fede4cf7e0efc
0xKenoSys <294089357@qq.com>
2026-07-04 21:29:31 +1000

    docs: explain the difference between C & Java

--- src/FindAllNumbersDisappeared/FindAllNumbersDisappeared.java
+++ src/FindAllNumbersDisappeared/FindAllNumbersDisappeared.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class FindAllNumbersDisappeared {
     public List<Integer> findDisappearedNumbers(int[] nums){
-        for (int i = 0; i < nums.length; i++){
+        for (int i = 0; i < nums.length; i++){  //区别：numsSize和nums.length
             int val = Math.abs(nums[i]);
             int idx = val - 1;
 
@@ -14,11 +14,22 @@ public class FindAllNumbersDisappeared {
             }
         }
 
-        List<Integer> result = new ArrayList<>();
+        List<Integer> result = new ArrayList<>();   //尖括号 <Integer> 叫泛型（Generics）。int 要写成 Integer。
+                                                    //最后空尖括号 <> 和圆括号 () 是固定搭配，告诉机器：“立刻执行造数组的动作，初始大小你看着办就行，反正以后不够用了你会自动扩容。”
+    /*这一行相当于：
+    int count = 0;
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] > 0) {
+            count++;
+        }
+    }
+    int* result = (int*)malloc(count * sizeof(int));
+    *returnSize = count;
 
+    */
         for (int i = 0; i < nums.length; i++){
             if (nums[i] > 0){
-                result.add(i + 1);
+                result.add(i + 1);  //相当于result[resultIndex] = i + 1;和resultIndex++;两行
             }
         }
 
