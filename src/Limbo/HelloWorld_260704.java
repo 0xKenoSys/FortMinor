e4476c87e0a453b6b3a748cb0a627eecdc9da312
0xKenoSys <294089357@qq.com>
2026-07-04 19:08:09 +1000

    first commit

---
+++ src/Limbo/HelloWorld_260704.java
@@ -0,0 +1,7 @@
+package Limbo;
+
+public class HelloWorld_260704 {
+    public static void main(String[] args){
+        System.out.println("You know she couldn't stay with you. You know she had to go.");
+    }
+}
