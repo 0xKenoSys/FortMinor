2a3a92b3f29079efcb93fc33fa3fca4396f9f572
0xKenoSys <294089357@qq.com>
2026-07-05 17:56:59 +1000

    docs: add 2nd fragment of comp1110

---
+++ src/Comp1110_01/Frag02.java
@@ -0,0 +1,13 @@
+package Comp1110_01;
+
+public class Frag02 {
+    public static void main(String[] args){
+        int i = 3;
+        int j =  i + 1;
+        System.out.println("The value of i is " + i + ".");
+        System.out.println("The value of j is " + j + ".");
+        if ( j > 2 ){
+            System.out.println("greater than 2");
+        }
+    }
+}
